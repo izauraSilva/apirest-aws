3845344bb5ee867ae9b29147cb20904000f27f26
izaura <pauloh.alves@castgrouppartner.com.br>
2020-04-29 23:39:50 -0300

    primeiro commit

---
+++ rest-with-aws/src/test/java/br/com/izatec/restwithaws/RestWithAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.izatec.restwithaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
